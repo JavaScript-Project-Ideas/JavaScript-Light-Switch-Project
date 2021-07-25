761424cb29fd5a4f75e29eaa0dca1715c296062c
Asif Adib <asifadib780@gmail.com>
2021-07-25 11:12:56 +0600

    added markup & Style

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// implementation of turnOnOff function -->
+function turnOnOff() 
+// taking image in image variable
+var image = document.getElementById('Image');
+
+//Match the image name
+//whether it is ONbulb or OFFbulb
+//change image to OFFbulb.jpg if
+//it match with ONbulb otherwise
+//change it to ONbulb.jpg -->
+if (image.src.match("ONbulb")){
+    image.src = "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+else
+    image.src = "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg",
+}
+
+
+
+
+// SEcond 
+const light = document.querySelector('#bulb');
+
+light.addEventListener('click', function(e){
+
+    e.target.classList.toggle('bulb-on');
+
+})
+
+
