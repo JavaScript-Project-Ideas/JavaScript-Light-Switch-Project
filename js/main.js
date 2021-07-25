86d3bd2e9e7f1464237db1764669629f24902b37
Asif Adib <asifadib780@gmail.com>
2021-07-25 17:34:20 +0600

    added new markup

--- js/main.js
+++ js/main.js
@@ -1,38 +1,14 @@
-// implementation of turnOnOff function -->
-function turnOnOff() 
-// taking image in image variable
-var image = document.getElementById('Image');
+// Main Logic Here
 
-//Match the image name
-//whether it is ONbulb or OFFbulb
-//change image to OFFbulb.jpg if
-//it match with ONbulb otherwise
-//change it to ONbulb.jpg -->
-if (image.src.match("ONbulb")){
-    image.src = "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
-else
-    image.src = "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg",
+function changeImage() {
+  var image = document.getElementById('#light');
+  if (image.src.match("bulbon")) {
+    image.src = "pic_bulboff.gif";
+  } else {
+    image.src = "pic_bulbon.gif";
+  }
 }
 
 
 
-
-// SEcond 
-const light = document.querySelector('#bulb');
-
-light.addEventListener('click', function(e){
-
-    e.target.classList.toggle('bulb-on');
-
-})
-
-
-// Main Logic Here
-function lightSwitch() {
-    var image = document.getElementById('light');
-    if (image.src.match("bulbon")) {
-      image.src = "../img/lightOff.gif";
-    } else {
-      image.src = "../img/lightOn.gif";
-    }
-  }
+document.getElementById('light').innerHTML = '<img src="../img/lightOff.gif">';
