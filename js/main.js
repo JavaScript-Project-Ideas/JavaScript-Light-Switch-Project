7cf1c4aaa7076651da131773b026ab861c342c87
Asif Adib <asifadib780@gmail.com>
2021-07-25 17:48:51 +0600

    added html js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 // Main Logic Here
 
-function changeImage() {
-  var image = document.getElementById('#light');
-  if (image.src.match("bulbon")) {
-    image.src = "pic_bulboff.gif";
-  } else {
-    image.src = "pic_bulbon.gif";
-  }
-}
 
-
-
-document.getElementById('light').innerHTML = '<img src="../img/lightOff.gif">';
