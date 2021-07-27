0185a55b2c34e065e9add707953e8c45fcaceed0
Asif Adib <asifadib780@gmail.com>
2021-07-27 10:01:07 +0600

    Add JS Logic here

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 // Main Logic Here
+let  lightOff = document.getElementById('lightoff');
+let  lightOn = document.getElementById('lighton');
 
 
+lightOff.addEventListener('click', function (e) {
+    document.getElementById('lightswitch').src = "../img/lightOff.gif"
+});
+
+lightOn.addEventListener('click', function (e) {
+    document.getElementById('lightswitch').src = "../img/lightOn.gif"
+});
