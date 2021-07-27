4e492e13cc9af79da04e00cd320c0a3e1dfd48a2
Asif Adib <asifadib780@gmail.com>
2021-07-27 10:07:10 +0600

    remove extra

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // Main Logic Here
-let lightOff = document.getElementById('lightoff');
-let lightOn = document.getElementById('lighton');
-
+let lightOff = document.getElementById('lightoff')
+let lightOn = document.getElementById('lighton')
 
 lightOff.addEventListener('click', function (e) {
     document.getElementById('lightswitch').src = "../img/lightOff.gif"
-});
-
+})
 lightOn.addEventListener('click', function (e) {
     document.getElementById('lightswitch').src = "../img/lightOn.gif"
-});
+})
